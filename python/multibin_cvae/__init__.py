d3ad19bd8edb1507cf6454d5d5ac4f26ed7bbe10
jarrod-dalton <jarrod-dalton@users.noreply.github.com>
2025-12-02 08:31:51 -0500

    Update __init__.py

--- python/multibin_cvae/__init__.py
+++ python/multibin_cvae/__init__.py
@@ -1,34 +1,27 @@
+"""
+Public API for the multibin_cvae package.
+
+Most users will interact with:
+
+- simulate_cvae_data(...)
+- CVAETrainer
+- fit_cvae_with_tuning(...)
+"""
 
 from .model import (
-    XYDataset,
-    MultivariateBinaryCVAE,
+    MultivariateOutcomeCVAE,
     CVAETrainer,
     tune_cvae_random_search,
+    tune_cvae_tpe,
+    fit_cvae_with_tuning,
 )
-
-from .simulate import (
-    simulate_cvae_data,
-    train_val_test_split,
-    summarize_binary_matrix,
-    compare_real_vs_generated,
-)
+from .simulate import simulate_cvae_data
 
 __all__ = [
+    "MultivariateOutcomeCVAE",
     "CVAETrainer",
-    "simulate_cvae_data",
-    "simulate_bernoulli_data",
-    "simulate_gaussian_data",
-    "simulate_poisson_data",
-    "summarize_binary_matrix",
-    "compare_real_vs_generated",
+    "tune_cvae_random_search",
+    "tune_cvae_tpe",
     "fit_cvae_with_tuning",
+    "simulate_cvae_data",
 ]
-
-try:
-    from importlib.metadata import version, PackageNotFoundError
-    try:
-        __version__ = version("multioutcome-cvae")
-    except PackageNotFoundError:
-        __version__ = "0.0.0"
-except Exception:
-    __version__ = "0.0.0"
